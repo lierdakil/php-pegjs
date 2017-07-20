5d29692137a167e22c58f95d759b21baf8d27a92
Nikolay Yakimov <root@livid.pp.ru>
2017-07-20 19:26:15 +0300

    Fix text() right bound

--- src/passes/generate-php.js
+++ src/passes/generate-php.js
@@ -495,7 +495,7 @@ module.exports = function(ast, options) {
     parts.push([
         '',
         '    private function text() {',
-        '      return substr($this->input, $this->peg_reportedPos, $this->peg_reportedPos + $this->peg_currPos);',
+        '      return substr($this->input, $this->peg_reportedPos, $this->peg_currPos - $this->peg_reportedPos);',
         '    }',
         '',
         '    private function offset() {',
